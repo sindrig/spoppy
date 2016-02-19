e193a8fbeadb48d6fe516a9a5c0a5e4fc89c45cf
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2016-02-19 19:01:16 +0000

    Ensure only one instance can run
    This fixes #30

--- spoppy.py
+++ spoppy.py
@@ -1,7 +1,7 @@
 import logging
-import traceback
 
 import click
+from lockfile import LockFile, LockTimeout
 # Ignore error, logging set up in logging utils
 from spoppy import logging_utils
 from spoppy.navigation import Leifur
@@ -12,32 +12,31 @@ logger = logging.getLogger('spoppy.main')
 @click.command()
 @click.argument('username', envvar='SPOPPY_USERNAME')
 @click.argument('password', envvar='SPOPPY_PASSWORD')
-@click.option('--debug', default=False)
-def main(username, password, debug):
-    navigator = Leifur(username, password)
-    if debug:
-        try:
-            navigator.start()
-        except Exception:
-            traceback.print_exc()
-            logger.error(traceback.format_exc())
-        finally:
-            navigator.shutdown()
-            logger.debug('Finally, bye!')
-    else:
-        try:
-            navigator.start()
-        finally:
-            navigator.shutdown()
-            logger.debug('Finally, bye!')
+def main(username, password):
+    try:
+        navigator = Leifur(username, password)
+        navigator.start()
+    finally:
+        navigator.shutdown()
+        logger.debug('Finally, bye!')
 
 
 if __name__ == '__main__':
+    lock = LockFile('/tmp/spoppy.lock')
     try:
-        main()
-    except click.exceptions.MissingParameter:
-        click.echo(
-            'You must either set the SPOPPY_USERNAME and SPOPPY_PASSWORD '
-            'environment variables or add username and password to your '
-            'the script\'s parameters!'
-        )
+        # Try for 5s to acquire the lock
+        lock.acquire(5)
+    except LockTimeout:
+        click.echo('Could not acquire lock, is spoppy running?')
+    else:
+        try:
+            main()
+        except click.exceptions.MissingParameter:
+            click.echo(
+                'You must either set the SPOPPY_USERNAME and SPOPPY_PASSWORD '
+                'environment variables or add username and password to your '
+                'the script\'s parameters!'
+            )
+    finally:
+        if lock.i_am_locking():
+            lock.release()
