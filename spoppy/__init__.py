266581037362a5357430a29e2d0dc89422886f82
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2016-03-07 18:25:35 +0000

    Make development work again
    This fixes #60

--- spoppy/__init__.py
+++ spoppy/__init__.py
@@ -1,2 +1,44 @@
+import logging
+
+import click
+from lockfile import LockFile, LockTimeout
+
+logger = logging.getLogger('spoppy.main')
+
+
 def get_version():
     return '1.0.2'
+
+
+@click.command()
+@click.argument('username', required=False)
+@click.argument('password', required=False)
+def main(username, password):
+    # Ignore error, logging set up in logging utils
+    from . import logging_utils
+    from .navigation import Leifur
+    from .config import get_config, set_config, get_config_from_user
+
+    lock = LockFile('/tmp/spoppy.lock')
+    try:
+        # Try for 5s to acquire the lock
+        lock.acquire(5)
+    except LockTimeout:
+        click.echo('Could not acquire lock, is spoppy running?')
+    else:
+
+        if username and password:
+            set_config(username, password)
+        else:
+            username, password = get_config()
+        if not (username and password):
+            username, password = get_config_from_user()
+        try:
+            navigator = Leifur(username, password)
+            navigator.start()
+        finally:
+            navigator.shutdown()
+            logger.debug('Finally, bye!')
+    finally:
+        if lock.i_am_locking():
+            lock.release()
